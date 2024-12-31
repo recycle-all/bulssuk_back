c7e971bd0fadf1d95ffae7cc36318f8a61bb17b1
hyeyeon-34 <hyee092@gmail.com>
2024-12-31 18:15:36 +0900

    Update calendarRoute.js

--- routes/user/calendarRoute.js
+++ routes/user/calendarRoute.js
@@ -7,16 +7,24 @@ const {
   updateAlarm,
   getAlarmsByDate,
   deactivateAlarm,
+  updateAttendanceAndPoints,
+  getAttendances,
+  getMonthImage,
+  getEvent,
 } = require('../../controllers/user/calendarController');
 
 const router = express.Router();
 
 router.get('/attendance/:user_id', getAttendance);
-router.post('/attendance', updateAttendance);
+// router.post('/attendance', updateAttendance);
 router.post('/alarm', addAlarm);
 router.get('/alarm/:user_id', getAlarms);
 router.put('/update_alarm', updateAlarm);
 router.get('/date', getAlarmsByDate);
 router.put('/deactivate_alarm', deactivateAlarm);
-
+router.post('/attendance',updateAttendanceAndPoints)
+router.get('/attendance/:user_no/:year/:month',getAttendances)
+router.get('/month_image/:month', getMonthImage)
+router.get('/get_event/:year/:month', getEvent)
 module.exports = router;
+
