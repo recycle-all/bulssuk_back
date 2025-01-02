61f2a64aa030b5e6f468ff31ad8fc859e37aa0b7
BY1502 <gunu110@naver.com>
2025-01-02 12:08:22 +0900

    Update faqController.js

--- controllers/admin/faqController.js
+++ controllers/admin/faqController.js
@@ -1,23 +1,5 @@
 const database = require('../../database/database');
-const cron = require('node-cron');
 const axios = require('axios');
-
-// 매주 월요일 오전 9시에 실행
-cron.schedule('* * * 1 *', async () => {
-  try {
-    console.log('Scheduler triggered: Generating FAQ data');
-    const response = await axios.post('http://222.112.27.120:8001/generate');
-    console.log('Scheduler response:', response.data);
-  } catch (error) {
-    console.error('Error in scheduler:', error);
-  }
-});
-
-// 첫 번째 */5: 매 5분마다.
-// 두 번째 *: 매 시간.
-// 세 번째 *: 매일.
-// 네 번째 *: 매월.
-// 다섯 번째 *: 요일 무관.
 
 // 모든 문의내역 가져오기
 exports.getAllFaq = async (req, res) => {
