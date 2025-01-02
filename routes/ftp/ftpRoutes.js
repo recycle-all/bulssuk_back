0d6ee56344d616a5744a0ed0aecc3cea95145cba
guswlda <guswl71945@gmail.com>
2025-01-02 20:17:34 +0900

    Delete routes/ftp/ftpRoutes.js

--- routes/ftp/ftpRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ftpController = require('../../controllers/ftp/ftpController');
-
-// FTP 이미지 요청 라우트
-router.get('/images/:filename', ftpController.getImage);
-
-module.exports = router;
