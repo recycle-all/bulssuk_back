085a68e83d1ac322d7933dfb3edddb5b84f591a4
Geonu <gunu110@naver.com>
2025-01-07 14:49:21 +0900

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 // 매월 1일 자정에 실행
-cron.schedule('42 * * * *', async () => {
+cron.schedule('52 * * * *', async () => {
   try {
     console.log('Scheduler triggered: Generating FAQ data');
     const response = await axios.post('http://222.112.27.120:8001/generate');
