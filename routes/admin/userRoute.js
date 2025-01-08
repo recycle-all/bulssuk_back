8bab42cc0ece072f50d03833164ed44f8b819789
hyeyeon-34 <hyee092@gmail.com>
2025-01-08 14:41:32 +0900

    Update userRoute.js

--- routes/admin/userRoute.js
+++ routes/admin/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUser, getPoint } = require('../../controllers/admin/userController');
+const { getUsers, getUser, getPoint, getAttendanceRate } = require('../../controllers/admin/userController');
 
 // 고객정보 조회
 router.get('/users', getUsers);
 router.get('/user/:user_no', getUser);
 // 포인트 조회
 router.get('/point', getPoint)
-
+// 출석률 조회
+router.get('/attendance_rate', getAttendanceRate)
 module.exports = router;
